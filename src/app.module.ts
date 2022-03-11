56de2c09be985afc8a0e55914546290b247d1fbd
Shubham Shukla <shubhamshukla@Shubham-Shukla.local>
2022-03-11 12:09:32 -0500

    working with index.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { PrismaService } from './prisma.service';
+import { PasswordProvider } from './providers/password';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [PrismaService, AppService, UsersService, PasswordProvider],
 })
 export class AppModule {}
